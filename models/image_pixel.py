5f83584b55f3c4f62852fed329d9a69ac3319e3c
lstrichashl <lstrichashl@gmail.com>
2016-07-18 18:59:03 +0300

    there are only 4 neighbors (north, east, south, west)

--- models/image_pixel.py
+++ models/image_pixel.py
@@ -4,36 +4,47 @@ from models.gradient import Gradient
 
 class ImagePixel:
 
-    def __init__(self, value=-1,
-                 neighbors=numpy.array([[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]),
+    def __init__(self,
+                 value=-1,
+                 north=-1,
+                 east=-1,
+                 south=-1,
+                 west=-1,
                  gradient_horizontal=Gradient(),
                  gradient_vertical=Gradient()):
         self.value = value
         self.energy_value = -1
-        self.neighbors = neighbors
+        self.__north = north
+        self.__east = east
+        self.__south = south
+        self.__west = west
         self.gradient_horizontal = gradient_horizontal
         self.gradient_vertical = gradient_vertical
 
-    def north_west(self):
-        return self.neighbors[0][0]
-
     def north(self):
-        return self.neighbors[0][1]
-
-    def north_east(self):
-        return self.neighbors[0][2]
+        return self.__north
 
     def east(self):
-        return self.neighbors[1][2]
-
-    def south_east(self):
-        return self.neighbors[2][2]
+        return self.__east
 
     def south(self):
-        return self.neighbors[2][1]
-
-    def south_west(self):
-        return self.neighbors[2][0]
+        return self.__south
 
     def west(self):
-        return self.neighbors[1][0]
+        return self.__west
+
+    def set_north(self, north):
+        self.__north = north
+        return self
+
+    def set_east(self, east):
+        self.__east = east
+        return self
+
+    def set_south(self, south):
+        self.__south = south
+        return self
+
+    def set_west(self, west):
+        self.__west = west
+        return self
