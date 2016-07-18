5cb317ada2149a0071450e7b843bd5a16ffc25a5
lstrichashl <lstrichashl@gmail.com>
2016-07-18 20:39:38 +0300

    make SeamCarvingImage be iterated

--- models/seam_carving_image.py
+++ models/seam_carving_image.py
@@ -4,5 +4,23 @@ from models.Iterator.VerticalIterator import VerticalIterator
 
 class SeamCarvingImage:
     def __init__(self, image_pixel_array):
+        self.head = image_pixel_array[0][0]
+        self.__current_horizontal_iter_pixel = self.head
+        self.__current_vertical_iter_pixel = self.head
         self.horizontal_iterator = HorizontalIterator(image_pixel_array[len(image_pixel_array) - 1][0])
         self.vertical_iterator = VerticalIterator(image_pixel_array[0][len(image_pixel_array[0]) - 1])
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.__current_horizontal_iter_pixel.east() is not None:
+            self.__current_horizontal_iter_pixel = self.__current_horizontal_iter_pixel.east()
+            return self.__current_horizontal_iter_pixel
+        elif self.__current_vertical_iter_pixel.south() is not None:
+            self.__current_vertical_iter_pixel = self.__current_vertical_iter_pixel.south()
+            self.__current_horizontal_iter_pixel = self.__current_vertical_iter_pixel
+            return self.__current_horizontal_iter_pixel
+        else:
+            self.__current_horizontal_iter_pixel = self.head
+            raise StopIteration()
--- seam_carving.py
+++ seam_carving.py
@@ -1,6 +1,3 @@
-import numpy
-from models.Iterator.HorizontalIterator import HorizontalIterator
-from models.Iterator.VerticalIterator import VerticalIterator
 from models.image_pixel import ImagePixel
 from models.seam_carving_image import SeamCarvingImage
 
@@ -11,48 +8,36 @@ def main(regular_image):
 
 def regular_image_to_seam_carving_image(regular_image):
     image_pixel_array = regular_image_to_image_pixel_array(regular_image)
+    image = SeamCarvingImage(image_pixel_array)
     set_image_neighbors(image_pixel_array)
-    set_image_gradients(image_pixel_array)
-    return SeamCarvingImage(image_pixel_array)
+    set_image_gradients(image)
+    return image
 
 
-def set_image_gradients(image_pixel_array):
-    for row in image_pixel_array:
-        for pixel in row:
-            pixel.gradient_horizontal.update()
-            pixel.gradient_vertical.update()
-
-
-def set_image_neighbors(image_pixel_array):
-    for row_index, row in enumerate(image_pixel_array):
-        for column_index, pixel in enumerate(row):
-            set_pixel_neighbors(pixel, column_index, row_index)
-
-
-def set_pixel_neighbors(self, pixel, column_index, row_index):
-    neighbors = self.get_neighbors(row_index, column_index)
-    pixel.set_north(neighbors[0][1])
-    pixel.set_east(neighbors[1][2])
-    pixel.set_south(neighbors[2][1])
-    pixel.set_west(neighbors[1][0])
-
-
-def regular_image_to_image_pixel_array(self, image):
+def regular_image_to_image_pixel_array(image):
     return [[ImagePixel(value=image[row_index][column_index])
              for column_index, pixel in enumerate(row)]
             for row_index, row in enumerate(image)]
 
 
-def get_neighbors(self, row_index, column_index):
-    neighbors = numpy.array([[None, None, None],
-                             [None, None, None],
-                             [None, None, None]])
+def set_image_gradients(image):
+    for pixel in image:
+        pixel.gradient_horizontal.update()
+        pixel.gradient_vertical.update()
+
+
+def set_image_neighbors(image_pixel_array):
+    for row_index, row in enumerate(image_pixel_array):
+        for column_index, pixel in enumerate(row):
+            set_pixel_neighbors(pixel, image_pixel_array, row_index, column_index)
+
+
+def set_pixel_neighbors(pixel, array, row_index, column_index):
     if row_index > 0:
-        neighbors[0][1] = self.array[row_index - 1][column_index]
-    if row_index < len(self.array):
-        neighbors[2][1] = self.array[row_index + 1][column_index]
+        pixel.set_north(array[row_index - 1][column_index])
+    if row_index < len(array):
+        pixel.set_south(array[row_index + 1][column_index])
     if column_index > 0:
-        neighbors[1][0] = self.array[row_index][column_index - 1]
-    if column_index < len(self.array[0]):
-        neighbors[1][2] = self.array[row_index][column_index + 1]
-    return neighbors
+        pixel.set_east(array[row_index][column_index - 1])
+    if column_index < len(array[0]):
+        pixel.set_west(array[row_index][column_index + 1])
